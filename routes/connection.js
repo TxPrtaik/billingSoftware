71b2646db6ed67d42f950bac5f15a956b2bf2512
pratik chindhe <169028142+TxPrtaik@users.noreply.github.com>
2025-01-01 13:56:58 +0530

    Update connection.js

--- routes/connection.js
+++ routes/connection.js
@@ -3,10 +3,10 @@ var mysql = require('mysql')
 var util = require('util')
 
 var conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "medical_db"
+    host: "brewde32qo0wqjng6viz-mysql.services.clever-cloud.com",
+    user: "ukzu3zsxjttwu1dc",
+    password: "MsUp3MINAlwci2tp93J2",
+    database: "brewde32qo0wqjng6viz"
 })
 
 var exe = util.promisify(conn.query).bind(conn)
